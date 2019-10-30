0fcdd9fc62e6c57159310826ff6cc73735094249
Benshuai Xu <bxu50@student.monash.edu>
2019-10-31 01:28:57 +1100

    build update

---
+++ build/precache-manifest.02d022faee21b7d8f358a8012958bf9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8d51895031cb39bf99a7168737ee66a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ad10c848965daeaef3c",
+    "url": "/static/css/2.a35a71c9.chunk.css"
+  },
+  {
+    "revision": "8c7dba194e2288df8a17",
+    "url": "/static/css/main.38bb7328.chunk.css"
+  },
+  {
+    "revision": "4ad10c848965daeaef3c",
+    "url": "/static/js/2.c9ec6f16.chunk.js"
+  },
+  {
+    "revision": "8c7dba194e2288df8a17",
+    "url": "/static/js/main.a2e9bd1a.chunk.js"
+  },
+  {
+    "revision": "731c44e584371012de71",
+    "url": "/static/js/runtime-main.ff0a1fb2.js"
+  }
+]);
